d29d31a1e670fdbb4b8118b5f08116b543d9c606
RhofirAbdelali <abdo.rhofir@gmail.com>
2025-03-04 12:35:39 +0100

    Initialisation du projet

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const app = express();
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(3000, () => {
+  console.log("Swagger disponible sur http://localhost:3000/api-docs");
+});
